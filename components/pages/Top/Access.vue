21f6100fc606c37c7bb2ef345aff136d8e02e85c
t.goto <t.goto@chatbox-inc.com>
2018-05-17 14:32:02 +0900

    update

--- components/pages/Top/Access.vue
+++ components/pages/Top/Access.vue
@@ -3,11 +3,7 @@
     <div class="home-Access_Wave"></div>
     <div class="st-Container">
       <h2 class="home-Access_Title">ACCESS</h2>
-      <no-ssr>
-        <googlemaps-map :center="center" :zoom="14" class="home-Access_Map">
-          <googlemaps-marker :position="marker.position"/>
-        </googlemaps-map>
-      </no-ssr>
+      <div class="home-Access_mapImage"/>
       <div class="home-Access_Description">
         <p class="home-Access_Place">グランフロント大阪&nbsp;北館&nbsp;タワーC&nbsp;8階</p>
         <p class="home-Access_Address">〒530-0011&nbsp;大阪市北区大深町3-1<br>&nbsp;グランフロント大阪&nbsp;北館&nbsp;タワーC8階</p>
@@ -48,7 +44,7 @@
     },
     methods: {
       ready () {
-        this.$refs.map.resize()
+        // this.$refs.map.resize()
       }
     }
   }
@@ -76,6 +72,16 @@
     &_Title {
       @include secTitle;
       margin-bottom: 30px;
+    }
+
+    &_mapImage {
+      height: 400px;
+      width: 80%;
+      background: url('https://maps.googleapis.com/maps/api/staticmap?center=osaka+umeda&zoom=14&size=600x300&maptype=roadmap&markers=34.705988,135.494489&key=AIzaSyDhk2ticWQZozS023qJqIO59333dfo-YPI');
+      background-size: cover;
+      margin-left: auto;
+      margin-right: auto;
+      margin-bottom: 2em;
     }
 
     &_Map {
