670bee1e2bb6ddf3f7f84aea9444172ba9b6253c
t.goto <t.goto@chatbox-inc.com>
2018-05-17 14:19:17 +0900

    update

--- components/pages/Top/Sponsor.vue
+++ components/pages/Top/Sponsor.vue
@@ -7,25 +7,25 @@
         <div class="home-Sponsor_Platinum">
           <h3>プラチナスポンサー</h3>
           <a class="home-Sponsor_Large" v-for="(sponsor,index) in platinum" :key="index" :href="sponsor.link" target="_blank">
-            <img class="home-Sponsor_Image" :src="'/images/sponsors/platinum/'+sponsor.image" alt="">
+            <img class="home-Sponsor_Image" :src="'/images/sponsors/platinum/'+sponsor.image" :title="sponsor.image">
           </a>
         </div>
         <div class="home-Sponsor_Gold">
           <h3>ゴールドスポンサー</h3>
           <a class="home-Sponsor_Medium" v-for="(sponsor,index) in gold" :key="index" :href="sponsor.link" target="_blank">
-            <img class="home-Sponsor_Image" :src="'/images/sponsors/gold/'+sponsor.image" alt="">
+            <img class="home-Sponsor_Image" :src="'/images/sponsors/gold/'+sponsor.image" :title="sponsor.image">
           </a>
         </div>
         <div class="home-Sponsor_Silver">
           <h3>シルバースポンサー</h3>
           <a class="home-Sponsor_Small" v-for="(sponsor,index) in silver" :key="index" :href="sponsor.link" target="_blank">
-            <img class="home-Sponsor_Image" :src="'/images/sponsors/silver/'+sponsor.image" alt="">
+            <img class="home-Sponsor_Image" :src="'/images/sponsors/silver/'+sponsor.image" :title="sponsor.image">
           </a>
         </div>
         <div class="home-Sponsor_Media">
           <h3>メディアスポンサー</h3>
           <a class="home-Sponsor_Small" v-for="(sponsor,index) in media" :key="index" :href="sponsor.link" target="_blank">
-            <img class="home-Sponsor_Image" :src="'/images/sponsors/media/'+sponsor.image" alt="">
+            <img class="home-Sponsor_Image" :src="'/images/sponsors/media/'+sponsor.image" :title="sponsor.image">
           </a>
         </div>
       </div>
