08b47621cac5e971ed672e560e6a8d78ff45b890
iLLviA <31163716+iLLviA@users.noreply.github.com>
2018-05-24 13:41:24 +0900

    time_tableの作成 (#31)

---
+++ components/pages/TimeTable/time_table.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="home-Time">
+        <div class="st-Container">
+            <h2 class="home-Time_title">TIME TABLE</h2>
+            <p class="home-Time_description">初心者から上級者までが楽しめるセッションが目白押し!これから始めようと思っていたフレームワークや（仮）クリックするとセッションの詳細が確認できます。</p>
+            <div class="home-Time_table">
+                <div class="home-Time_item1"></div>
+                <div class="home-Time_item2"></div>
+                <div class="home-Time_item3"></div>
+                <div class="home-Time_item4"></div>
+                <div class="home-Time_item5"></div>
+                <div class="home-Time_item6"></div>
+                <div class="home-Time_item7"></div>
+            </div>
+            <div class="home-Time_OtherLink"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+    @import "~assets/scss/library/_variable.scss";
+    @import "~assets/scss/library/_mixin.scss";
+
+.home-Time {
+    padding: 70px 0;
+    background-color: #fff;
+    &_title {
+        color:#044ca4
+    }
+    &_description {
+        color: #044ca4;
+    }
+    &_table {
+        margin-top: 30px;
+        display: grid;
+        grid-template-rows: 20px 30px;
+        grid-template-columns: 25px 100px 100px 100px;
+        .home-Time_item1 {
+            grid-row: 1 / 3;
+            grid-column: 1 / 2;
+            background-color: #f4f4f4;
+        }
+        .home-Time_item2 {
+            grid-row: 1/2;
+            grid-column: 2/3;
+            background-color: #04c4ac;
+
+        }
+        .home-Time_item3 {
+            grid-row:2/3;
+            grid-column: 2/3;
+            background-color: #ccf4ec;
+        }
+        .home-Time_item4 {
+            grid-row:1/2;
+            grid-column: 3/4;
+            background-color: #fce41c;
+        }
+        .home-Time_item5 {
+            grid-row:2/3;
+            grid-column: 3/4;
+            background-color: #fcfcf4;
+        }
+        .home-Time_item6 {
+            grid-row:1/2;
+            grid-column: 4/5;
+            background-color: #084ca4;
+        }
+        .home-Time_item7 {
+            grid-row:2/3;
+            grid-column: 4/5;
+            background-color:#f8f4fc;
+        }
+    }
+}
+
+</style>
---
+++ pages/TimeTable.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <sec-table></sec-table>
+        <sec-share></sec-share>
+        <sec-follow></sec-follow>
+        <p-footer></p-footer>
+    </section>
+</template>
+
+<script>
+
+    import secTable from '~/components/pages/TimeTable/time_table.vue'
+    import secShare from '~/components/pages/Top/Share.vue'
+    import secFollow from '~/components/pages/Top/Follow.vue'
+    import pFooter from '~/components/Footer.vue'
+
+    export default{
+        components:{
+            secTable,
+            secShare,
+            secFollow,
+            pFooter
+
+        }
+    }
+
+</script>
+
+
+<style lang="scss">
+    @import "~assets/scss/foundation/_base.scss";
+    @import "~assets/scss/object/_common.scss";
+</style>
