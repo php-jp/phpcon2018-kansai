7e5767b0b993e6ec148cee1717c56e10b119c470
t.goto <t.goto@chatbox-inc.com>
2018-05-24 13:40:40 +0900

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,10 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'PHPカンファレンス関西2018',
+    titleTemplate: '%s - PHPカンファレンス関西2018',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: 'Nuxt.js project'},
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
