da00b85a09a872ffd33afbb628c9a9c639914f08
iLLviA <illvia.3594@gmail.com>
2018-06-06 02:54:14 +0900

    会場・受付追加

--- components/pages/TimeTable/time_table.vue
+++ components/pages/TimeTable/time_table.vue
@@ -25,6 +25,22 @@ export default {
   data() {
     return {
       sessions: [
+          {}
+          ,
+          {
+              tracka: {
+                  title:"開会挨拶",
+                  name:"太郎"
+              },
+              trackb: {
+                  title:"",
+                  name:""
+              },
+              trackc: {
+                  title:"",
+                  name:""
+              },
+          },
         {
           tracka: [
             this.$store.state.speakers.matuhuzi_hideharu,
@@ -90,47 +106,7 @@ export default {
     color: #044ca4;
     margin-bottom: 30px;
   }
-  &_table {
-    margin-top: 30px;
-    display: grid;
-    grid-template-rows: 20px 30px;
-    grid-template-columns: 25px 100px 100px 100px;
-    .home-Time_item1 {
-      grid-row: 1 / 3;
-      grid-column: 1 / 2;
-      background-color: #f4f4f4;
-    }
-    .home-Time_item2 {
-      grid-row: 1/2;
-      grid-column: 2/3;
-      background-color: #04c4ac;
-    }
-    .home-Time_item3 {
-      grid-row:2/3;
-      grid-column: 2/3;
-      background-color: #ccf4ec;
-    }
-    .home-Time_item4 {
-      grid-row:1/2;
-      grid-column: 3/4;
-      background-color: #fce41c;
-    }
-    .home-Time_item5 {
-      grid-row:2/3;
-      grid-column: 3/4;
-      background-color: #fcfcf4;
-    }
-    .home-Time_item6 {
-      grid-row:1/2;
-      grid-column: 4/5;
-      background-color: #084ca4;
-    }
-    .home-Time_item7 {
-      grid-row:2/3;
-      grid-column: 4/5;
-      background-color:#f8f4fc;
-    }
-  }
+
   &_session {
     margin-bottom: 3em;
     &Title {
